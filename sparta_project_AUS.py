a6cdce47b46be9781922f85c9b5cdc0da0fc9dd6
ariunaa13 <dongeun13@gmail.com>
2020-02-25 22:22:25 +0900

    0225_GPAUS

---
+++ sparta_project_AUS.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/73.0',
+}
+
+response = requests.get(
+    'https://www.greenpeace.org.au/blog/',
+    headers=headers,
+)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+articles = soup.select('article')
+
+for article in articles:
+    a_tag = article.select_one('h3 > a')
+    title = a_tag.text
+    article_link = a_tag['href']
+
+    date = article.select_one('div.teaser-date').text
+
+    print(f'{date} / {title} / {article_link}')
