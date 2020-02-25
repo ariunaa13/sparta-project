5259625ea928183f5d293c79551d6607cb7ced2f
ariunaa13 <dongeun13@gmail.com>
2020-02-25 21:02:32 +0900

    0225_GPUK_ING
    For 문 실행 안됨

---
+++ sparta_project_UK.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+headers = {
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/73.0',
+}
+
+response = requests.get(
+    'https://www.greenpeace.org.uk/latest-updates/',
+    headers=headers,
+)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+articles = soup.select('#main > div.main-section')
+
+for article in articles:
+    a_tag = article.select_one('article > header > h3 > a')
+    title = a_tag.text
+    article_link = a_tag['href']
+
+    date = article.select_one('time').text
+
+    print(f'{date} / {title} / "https://www.greenpeace.org.uk{article_link}"')
